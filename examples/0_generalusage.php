2af28ea76c469359b6c6155091d026bd1a5b0051
Vincent Mahnke <accounts@vimaster.de>
2015-07-15 21:35:46 +0200

    ~ thumbnailURL is now imageURL that represents the full-image-src ~ Fixed brackets, that invalidated code

--- examples/0_generalusage.php
+++ examples/0_generalusage.php
@@ -14,21 +14,21 @@
 				'taxonomy' => 'department',
 				'field'    => 'slug',
 				'terms'    => 'abteilung-1',
-			),
-		);
+			)
+		)
 		// Removing the part between this, and the comment above, removes the department-filter
 	));
 
 	// Itterate over every candidate
 	for($i = 0; $i < count($candidateQuery->posts); $i++) {
 		$post = $candidateQuery->posts[$i];
-		$thumbnailURL = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "full", false );
+		$imageURL = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "full", false );
 
 		// Useful values to echo here:
-		// $post->ID				(1337)
-		// $post->post_title		(Firstname Lastname)
-		// $post->post_content		(Description of the candidate)
-		// $thumbnailURL			(URL of the )
+		// $post->ID														(1337)
+		// $post->post_title												(Firstname Lastname)
+		// $post->post_content												(Description of the candidate)
+		// $imageURL														(URL of the full sized image of the candidate)
 	
 		// Since one candidate can be assigned multiple departments, we itterate again...
 		$departments = wp_get_post_terms($post->ID, 'department', array("fields" => "all"));
